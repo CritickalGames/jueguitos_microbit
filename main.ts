b24dd9b0622e82407e1a87862fcfe7e0f10d0b22
Braiam Rodriguez <31327715+CritickalGames@users.noreply.github.com>
2024-11-18 19:12:41 -0300

    El game over se cambió por uno propio

--- main.ts
+++ main.ts
@@ -11,6 +11,8 @@ function iniciar_juego () {
     Muro.set(LedSpriteProperty.Y, 0)
 }
 input.onGesture(Gesture.Shake, function () {
+    FIN_DEL_JUEGO = true
+    estado = false
     basic.showIcon(IconNames.Heart)
     iniciar_juego()
 })
@@ -22,6 +24,7 @@ function reinciar_muro () {
     Muro.set(LedSpriteProperty.X, randint(0, 4))
     Muro.set(LedSpriteProperty.Y, 0)
 }
+let FIN_DEL_JUEGO = false
 let jugador: game.LedSprite = null
 let estado = false
 let Muro: game.LedSprite = null
@@ -30,10 +33,11 @@ game.setLife(3)
 game.setScore(0)
 jugador = game.createSprite(2, 5)
 Muro = game.createSprite(0, 0)
+FIN_DEL_JUEGO = false
 basic.showString("SACUDE")
 basic.forever(function () {
     basic.pause(300)
-    if (estado == true) {
+    if (FIN_DEL_JUEGO) {
         Muro.change(LedSpriteProperty.Y, 1)
         if (Muro.isTouching(jugador)) {
             game.addScore(1)
@@ -41,10 +45,19 @@ basic.forever(function () {
             basic.showNumber(game.score())
             reinciar_muro()
         } else if (Muro.y() == 4) {
-            game.removeLife(1)
             music.play(music.tonePlayable(330, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
             basic.showNumber(game.life())
-            reinciar_muro()
+            if (game.life() - 1 != 0) {
+                game.removeLife(1)
+                reinciar_muro()
+            } else {
+                FIN_DEL_JUEGO = false
+                estado = true
+            }
         }
+    } else if (estado) {
+        basic.showString("GAME OVER")
+        basic.showString("SCORE")
+        basic.showNumber(game.score())
     }
 })
